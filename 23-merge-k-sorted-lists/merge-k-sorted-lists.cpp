1d372987b94766f19846c0ffad2d9323ac7c183d
Pawan Prajapati <138574514+Pawan318@users.noreply.github.com>
2026-08-09 16:50:37 +0530

    Time: 0 ms (100.00%) | Memory: 18.4 MB (82.61%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -8,49 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
- 
+//Approach-2 - (Using Priority Queue)
+//T.C : O(N * log(K)), N =  total nodes, K = total K lists
+//S.C : O(K),heap size never exceeds K
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode* l1, ListNode* l2) {
-        if(!l1)
-            return l2;
-        if(!l2)
-            return l1;
-
-        if(l1->val <= l2->val) {
-            l1->next = mergeTwoSortedLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoSortedLists(l1, l2->next);
-            return l2;
-        }
-        
-        return NULL;
-    }
-    
-    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists) {
-        if(start == end)
-            return lists[start];
-        
-        if(start > end)
-            return NULL;
-        
-        int mid = start + (end-start)/2;
-        
-        ListNode* l1 = partitionAndMerge(start, mid, lists);
-        ListNode* l2 = partitionAndMerge(mid+1, end, lists);
-        
-        return mergeTwoSortedLists(l1, l2);
-    }
-    
+    //T.C : O(N), N =. total nodes in all lists
+    //S,C : O(N)
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
-        int n = lists.size();
-        
-        if(n == 0)
+
+        auto lambda = [](ListNode* node1, ListNode* node2) {
+            return node1->val > node2->val;
+        };
+
+        priority_queue<ListNode*, vector<ListNode*>, decltype(lambda)> pq(lambda); 
+
+        for(ListNode* node : lists) {
+            if(node != NULL) {
+                pq.push(node);
+            }
+        }
+
+        if(pq.empty()) {
             return NULL;
-        
-        return partitionAndMerge(0, n-1, lists);
-        
+        }
+
+        ListNode* head = pq.top(); //head of final merged list
+        pq.pop();
+
+        if(head->next != NULL) {
+            pq.push(head->next);
+        }
+
+        ListNode* tail = head;
+
+        while(!pq.empty()) {
+            ListNode* curr = pq.top();
+            pq.pop();
+
+            tail->next = curr;
+            tail = tail->next;
+
+            if(curr->next != NULL)
+                pq.push(curr->next);
+        }
+
+        return head;
+
     }
 };
+
