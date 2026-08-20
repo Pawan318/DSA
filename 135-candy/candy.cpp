1b5b5220032a2e40f215cec950cbed34e84a5731
Pawan Prajapati <138574514+Pawan318@users.noreply.github.com>
2026-08-20 18:18:39 +0530

    Time: 3 ms (27.83%) | Memory: 68.1 MB (9.76%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,25 +1,50 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
+
         int n = ratings.size();
-        vector<int>count(n,1);
-        
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1]){
-                count[i] = max(count[i], count[i-1]+1);
+
+        if(n <= 1)
+            return n;
+
+        int result = 1;
+
+        int up = 0;
+        int down = 0;
+        int peak = 0;
+
+        for(int i = 1; i < n; i++) {
+
+            if(ratings[i] > ratings[i - 1]) {
+
+                up++;
+                down = 0;
+                peak = up;
+
+                result += up + 1;
+            }
+
+            else if(ratings[i] < ratings[i - 1]) {
+
+                down++;
+                up = 0;
+
+                result += down;
+
+                if(down > peak)
+                    result++;
+            }
+
+            else {
+
+                up = 0;
+                down = 0;
+                peak = 0;
+
+                result++;
             }
         }
 
-        for(int i = n-2; i >= 0; i--){
-            if(ratings[i] > ratings[i+1]){
-                count[i] = max(count[i], count[i+1]+1);
-            }
-        }
-
-        int result =  0;
-        for(int i = 0; i < n; i++){
-            result += count[i];
-        }
         return result;
     }
 };
